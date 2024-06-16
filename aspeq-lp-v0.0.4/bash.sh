9f75cc284658473ed9a770d124577274c36c61a7
gabrieldrouin <info@gabrieldrouin.com>
2024-06-15 20:15:19 -0400

    Initial commit from Create Next App

---
+++ aspeq-lp-v0.0.4/bash.sh
@@ -0,0 +1,11 @@
+# Use "chmod +x bash.sh && ./bash.sh" to run this script
+
+# https://ui.shadcn.com/docs/installation/next
+npx create-next-app@latest aspeq-lp --typescript --tailwind --eslint --app --src-dir --use-npm --import-alias "@/*"
+cd aspeq-lp
+yes | npx shadcn-ui@latest init
+
+# https://prettier.io/docs/en/install.html
+npm install --save-dev --save-exact prettier
+node --eval "fs.writeFileSync('.prettierrc','{}\n')"
+npx prettier . --write
